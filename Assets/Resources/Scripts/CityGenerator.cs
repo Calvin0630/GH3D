da5125f420a8538ae514ba21595a107232b0d18a
Liam Atticus Clarke <liam.att.clarke@gmail.com>
2015-12-19 19:52:58 -0500

    Added Bezier Function

---
+++ Assets/Resources/Scripts/CityGenerator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class CityGenerator {
+    public static void Generate() {
+
+    }
+
+    public static Vector3 BezierCurve(Vector3[] points, float t, int indexOffset) {
+        if (points.Length < 3) throw new UnityException("Points array must have at least 3 elements");
+
+        t = Mathf.Clamp(t, 0, 1f);
+
+        int segments = (points.Length - 1) / 2;
+
+        float segmentWidth = 1.0f / segments;
+        if (t <= segmentWidth * (indexOffset + 1)) {
+            t = (t % segmentWidth) / segmentWidth;
+            float oneMinusT = 1.0f - t;
+            int offset = indexOffset * 2;
+            return oneMinusT * oneMinusT * points[0 + offset] + 2 * oneMinusT * t * points[1 + offset] + t * t * points[2 + offset];
+        } else if (t <= 1.0f) {
+            return BezierCurve(points, t, indexOffset + 1);
+        } else {
+            return Vector3.zero;
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/Test.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+    float x = 0;
+    public Vector3[] points;
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = CityGenerator.BezierCurve(points, x, 0);
+        x += 0.005f;
+    }
+}
